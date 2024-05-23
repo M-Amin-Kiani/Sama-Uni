fbda528b9f6c71e34dafed480711f908b8a53f93
Amin Kiani <aminkiani82@gmail.com>
2024-05-23 14:45:18 +0330

    MVP 3 _ semi final

--- Backend/taApp/middleware.py
+++
@@ -1,7 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-
-class DisableCsrfCheck(MiddlewareMixin):
-    def process_request(self, request):
-        attr = '_dont_enforce_csrf_checks'
-        if not getattr(request, attr, False):
-            setattr(request, attr, True)
